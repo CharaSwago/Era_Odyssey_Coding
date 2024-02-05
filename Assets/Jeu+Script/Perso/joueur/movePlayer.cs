1ec7cb3b3e171371a514eb4f48167d5c6d9f8fdf
Malicia Torrent <malicia.torrent@gmail.com>
2024-02-05 14:16:49 +0100

    Dash Fonctionnel

--- Assets/Jeu+Script/Perso/joueur/movePlayer.cs
+++ Assets/Jeu+Script/Perso/joueur/movePlayer.cs
@@ -1,18 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class movePlayer : MonoBehaviour
 
 {
+    //Variable publique, mouvement du perso
     public float moveSpeed;
+    //Variable publique, saut + force du saut + savoir si le perso touche le sol
     public float jumpForce;
-
     public bool isJumping = false;
     public bool isGrounded;
 
+    //Variable pour le systeme du saut
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask collisionLayers;
-
+    //L'animation du saut
     public Animator animator;
     public SpriteRenderer spriteRenderer;
     public Rigidbody2D rb;
@@ -20,13 +24,37 @@ public class movePlayer : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     private float horizontalMovement;
 
+    public float dashingVelocity = 14f;
+    private float dashingTime = 0.5f; 
+    private Vector2 dashingDir;
+    private bool isDashing;
+    private bool canDashing = true;
+    [SerializeField] private TrailRenderer trailRenderer; 
+
     void Update()
     {
+
         horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
+            canDashing = true;
+        }
+
+        if (Input.GetButtonDown("Dash") && canDashing){
+            canDashing = false;
+            isDashing = true;
+            trailRenderer.emitting = true;
+            dashingDir = new Vector2(horizontalMovement, Input.GetAxis("Vertical"));
+            if(dashingDir == Vector2.zero){
+                dashingDir = new Vector2(transform.localScale.x, 0);
+            }
+            StartCoroutine(stopDashing());
+        }
+
+        if(isDashing){
+            rb.velocity = dashingDir.normalized * dashingVelocity;
         }
 
         Flip(rb.velocity.x);
@@ -38,7 +66,6 @@ public class movePlayer : MonoBehaviour
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
-
         MovePlayer(horizontalMovement);
     }
 
@@ -68,4 +95,11 @@ public class movePlayer : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
+
+    private IEnumerator stopDashing(){
+        yield return new WaitForSeconds(dashingTime);
+        trailRenderer.emitting = false;
+        isDashing = false;
+    }
+
 }
