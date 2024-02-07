87f25bb482f4c4eb4699cfd1b1819e5d296bb95e
Malicia Torrent <malicia.torrent@gmail.com>
2024-02-07 12:56:24 +0100

    Création d'un script spécial wall jump et wall slide

--- Assets/Jeu+Script/Perso/joueur/movePlayer.cs
+++ Assets/Jeu+Script/Perso/joueur/movePlayer.cs
@@ -10,35 +10,28 @@ public class movePlayer : MonoBehaviour
     public float wallSpeed;
     private bool isMoving;
 
-    // Variable publique, saut + force du saut + savoir si le perso touche le sol
+    // Variable publique du saut
     [Range(1,10)]
     public float jumpForce;
-    public bool isJumping = false;
+    private bool isJumping = false;
+
+    // Variable bool 
     private bool isGrounded;
     private bool isWall;
-    private float slideSpeed = 1.5f;
 
     // Variable pour le systeme d'accroche au mur 
     public Transform wallCheckRigth;
-    public float wallCheckRigthRadius;
+    private float wallCheckRigthRadius = 0.1f;
     public Transform wallCheckLeft;
-    public float wallCheckLeftRadius;
-    public bool wallGrab;
-
-    // Variable pour le systeme du saut sur le mur
-    private bool isWallJumping;
-    private float wallJumpingDirection;
-    public float wallJumpingTime;
-    private float WallJumpingCounter;
-    public float wallJumpingDuration;
-
+    private float wallCheckLeftRadius = 0.1f;
+    private bool wallGrab;
 
     // Variable pour le systeme du saut 
     public Transform groundCheck;
-    public float groundCheckRadius;
+    private float groundCheckRadius = 0.1f;
     public LayerMask collisionLayers;
 
-    // L'animation du saut
+    // L'animation
     public Animator animator;
     public SpriteRenderer spriteRenderer;
     public Rigidbody2D rb; 
@@ -46,8 +39,9 @@ public class movePlayer : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     private float horizontalMovement;
 
-    public float dashingVelocity = 5f;
-    private float dashingTime = 0.5f; 
+    // Variable du dash
+    private float dashingVelocity = 5f;
+    private float dashingTime = 0.3f; 
     private Vector2 dashingDir;
     private bool isDashing;
     private bool canDashing = true;
@@ -58,7 +52,7 @@ public class movePlayer : MonoBehaviour
 
         horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if(Input.GetButtonDown("Jump") && (isGrounded || isWall))
         {
             isJumping = true;
             canDashing = true;
@@ -73,14 +67,11 @@ public class movePlayer : MonoBehaviour
                 dashingDir = new Vector2(transform.localScale.x, 0);
             }
             StartCoroutine(stopDashing());
+            
         }
 
         if(isDashing){
             rb.velocity = dashingDir.normalized * dashingVelocity;
-        }
-
-        if(isWall && !isGrounded){
-            wallSide();
         }
 
         Flip(rb.velocity.x);
@@ -113,7 +104,7 @@ public class movePlayer : MonoBehaviour
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
-
+        
         if(isJumping == true)
         {
             rb.velocity = Vector2.up * jumpForce;
@@ -127,13 +118,17 @@ public class movePlayer : MonoBehaviour
             spriteRenderer.flipX = false;
         } else if (_velocity < -0.1f){
             spriteRenderer.flipX = true;
-        }
+        } 
     }
 
     private void OnDrawGizmos()
     {
+        // for ground check
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+        // for wall check
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(wallCheckLeft.position, wallCheckLeftRadius);
         Gizmos.DrawWireSphere(wallCheckRigth.position, wallCheckRigthRadius);
     }
@@ -144,9 +139,5 @@ public class movePlayer : MonoBehaviour
         isDashing = false;
     }
 
-    private void wallSide()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, -slideSpeed);
-    }
 
 }
